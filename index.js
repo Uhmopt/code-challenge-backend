46da4d5d69e57bc4d04aa7e66f454585c0abb34c
Uhmopt <qethum@gmail.com>
2021-11-28 18:33:05 +1100

    [New] deploy frontend

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const { ApolloServer, gql } = require("apollo-server");
+const express = require("express");
+const path = require("path");
+const { ApolloServer, gql } = require("apollo-server-express");
 const movies = require("./seeder/moviesSeeder.json");
 
 // A schema is a collection of type definitions (hence "typeDefs")
@@ -29,15 +31,27 @@ const resolvers = {
     movies: () => movies,
   },
 };
-
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
+
 const server = new ApolloServer({ typeDefs, resolvers });
 
-// The `listen` method launches a web server.
-server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-  console.log(`
-    🚀  Server is ready at ${url}
-    📭  Query at https://studio.apollographql.com/dev
+const app = express();
+
+app
+  .use(express.static(path.resolve(__dirname, "public")))
+  .get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "public", "index.html"));
+  });
+
+server.start().then(() => {
+  // apply
+  server.applyMiddleware({ app });
+
+  // The `listen` method launches a web server.
+  app.listen({ port: process.env.PORT || 4000 }, () => {
+    console.log(`
+    🚀  Server is ready 
   `);
+  });
 });
